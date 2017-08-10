e25d4f5d9efe059431b4b06b32820c1353c3aa83
Rich Peers <richpeers1@gmail.com>
2017-08-10 18:04:27 +0100

    recaptcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,10 @@
                             </div>
                         </div>
 
+                        @if(config('auth.captcha.registration'))
+                            @captcha()
+                        @endif
+
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
